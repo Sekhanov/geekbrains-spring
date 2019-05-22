03041d161dc0d03db03b9accd074660be9c96773
Sergey Khanov <sekhanov@gmail.com>
2019-05-22 21:00:44 +0300

    login page

--- geekbrains-spring/src/main/java/com/skhanov/geekbrainsspring/SecurityConfig.java
+++ geekbrains-spring/src/main/java/com/skhanov/geekbrainsspring/SecurityConfig.java
@@ -6,6 +6,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -28,6 +29,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		.password("{noop}asdf")
 //		.authorities("ROLE_USER");
 		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
+		.antMatchers("/", "/**").permitAll()
+		.and().formLogin().loginPage("/login")
+//		.loginProcessingUrl("/authenticateTheUser")
+		.permitAll();
 	}
 	
 	
