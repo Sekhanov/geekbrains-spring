6ccf56a505a5b406b5cccaa3ed6d9c907c43e80f
Sergey Khanov <sekhanov@gmail.com>
2019-05-23 19:05:33 +0300

    sec:authorize working

--- geekbrains-spring/src/main/java/com/skhanov/geekbrainsspring/SecurityConfig.java
+++ geekbrains-spring/src/main/java/com/skhanov/geekbrainsspring/SecurityConfig.java
@@ -4,11 +4,13 @@ package com.skhanov.geekbrainsspring;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -36,11 +38,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.antMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
 		.antMatchers("/", "/**").permitAll()
-		.and().formLogin().loginPage("/login")
-		.loginProcessingUrl("/authenticateTheUser")
-		.permitAll();
+		.and().formLogin().loginPage("/login").failureUrl("/accessDenied").loginProcessingUrl("/authenticateTheUser")		
+		.and().logout().logoutSuccessUrl("/");
 	}
 	
-	
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//	
 
 }
